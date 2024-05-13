570e372703ea1b56e19f661b52d9835db5094170
BruceBarigye <ndyomukingbruce@gmail.com>
2024-05-13 14:31:51 +0300

    added more modification

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import 'swiper/css';
 import './App.css';
 import Banner from './pages/Banner';
 import Header from './pages/Header';
+import Main from './pages/Main';
 
 function App() {
   return (
     <>
     <Header/>
     <Banner/>
-    
+    <Main/>
     </>
   );
 }
